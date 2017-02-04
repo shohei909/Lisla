0cb19fc3a313b09ef08d11ce044af67b47a5af04
shohei909 <909mmsphere@gmail.com>
2017-02-05 00:34:47 +0900

    ignore bin directory

--- impl/haxe/hxlitll/bin/test.js
+++
@@ -1,8 +0,0 @@
-(function (console) { "use strict";
-var TestMain = function() { };
-TestMain.main = function() {
-};
-TestMain.main();
-})(typeof console != "undefined" ? console : {log:function(){}});
-
-//# sourceMappingURL=test.js.map
